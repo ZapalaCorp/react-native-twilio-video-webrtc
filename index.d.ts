9ea1cae460bd66f02f7013401f6461798f3810c7
Daniel Souza <danielsvba@hotmail.com>
2022-07-06 11:29:25 -0300

    Change name of package in module type

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module "react-native-twilio-video-webrtc" {
+declare module "@zapala/react-native-twilio-video-webrtc" {
   import { ViewProps } from "react-native";
   import React from "react";
 
